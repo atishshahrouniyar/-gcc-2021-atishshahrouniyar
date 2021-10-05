e6f7917211bc4eb9a99c12befde57c02d5d65331
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2021-10-05 22:42:16 +0530

    COMMIT BY GCC 2021

--- Question5/Question5.cpp
+++ Question5/Question5.cpp
@@ -2,16 +2,24 @@ using namespace std;
 #include <string>
 #include <iostream>
 
-string solution(string n){
-    // code here
-    //return "A";
-    //return "B"
+string solution(string n) {
+    int previous = -1, max0 = 0;
+    for (int i = 0; i < n.length(); ++i) {
+        if (n[i] == '0')
+            max0 = max(max0, i - previous);
+        else
+            previous = i;
+    }
+    if (max0 & 1)
+        return "A";
+    else
+        return "B";
 }
 
 /*  Do not edit below code */
 int main() {
-	string  n;
-	cin >> n;	
-    string answer=solution(n);
-	cout << answer << endl;	
+    string  n;
+    cin >> n;
+    string answer = solution(n);
+    cout << answer << endl;
 }
