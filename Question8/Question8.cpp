1b6897eeb43ba46114ee84a1ea0b09ae425b2b16
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2021-10-17 20:10:39 +0530

    COMMIT BY GCC 2021

--- Question8/Question8.cpp
+++ Question8/Question8.cpp
@@ -1,117 +1,98 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-long long pos, max_profit, cur_profit, j;
 
 long long calculateMaximizedReturns(int n, int k, int d, long long m, vector<long long> returns) {
-	vector<int> status(n, 1);
-	for (int a = 0; a < k; ++a) {
-		pos = -1;
-		max_profit = INT_MIN;
-		for (int i = 0; i < n; ++i) {
-			cur_profit = 0;
-			if (status[i] == 0)
-				continue;
-			if (status[i] == 1) {
-				j = i + 1;
-				while (j <= i + d && j < n && status[j]) {
-					cur_profit += returns[j];
-					++j;
-				}
-				cur_profit *= m - 1;
-				cur_profit -= returns[i];
-			}
-			else {
-				j = i + 1;
-				while (j <= i + d && j < n && status[j] == 2) {
-					++j;
-					continue;
-				}
-				while (j <= i + d && j < n) {
-					cur_profit += returns[j];
-					++j;
-				}
-				cur_profit *= m - 1;
-				cur_profit -= returns[i] * m;
-			}
-			if (cur_profit > max_profit) {
-				pos = i;
-				max_profit = cur_profit;
-			}
-		}
-		status[pos] = 0;
-		j = pos + 1;
-		while (j <= pos + d && j < n && status[j]) {
-			status[j] = 2;
-			++j;
-		}
-	}
-	max_profit = 0;
-	for (int i = 0; i < n; ++i) {
-		if (status[i] == 1)
-			max_profit += returns[i];
-		else if (status[i] == 2)
-			max_profit += m * returns[i];
-	}
-	return max_profit;
+    long long pos, profit, current;
+    vector<long long> status(n, 1);
+    for (int i = 0; i < k; ++i) {
+        pos = -1; profit = INT_MIN;
+        for (int j = 0; j < n; ++j) {
+            if (status[j]) {
+                current = (-1) * status[j] * returns[j];
+                for (int k = j + 1; k <= j + d && k<n; ++k) {
+                    if (!status[k]) {
+                        break;
+                    }
+                    else if (status[k] == 1) {
+                        current += (m - 1) * returns[k];
+                    }
+                }
+            }
+            if (current > profit) {
+                profit = current;
+                pos = j;
+            }
+        }
+        status[pos] = 0;
+        for (int j = pos + 1; j <= pos + d && j < n; ++j) {
+            if(status[j])
+                status[j] = m;
+        }
+    }
+    profit = 0;
+    for (int i = 0; i < n; ++i) {
+        profit += status[i] * returns[i];
+    }
+    return profit;
 }
 
 vector<string> split(const string& str, char delim) {
-	vector<string> strings;
-	size_t start;
-	size_t end = 0;
-	while ((start = str.find_first_not_of(delim, end)) != string::npos) {
-		end = str.find(delim, start);
-		strings.push_back(str.substr(start, end - start));
-	}
-	return strings;
+    vector<string> strings;
+    size_t start;
+    size_t end = 0;
+    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
+        end = str.find(delim, start);
+        strings.push_back(str.substr(start, end - start));
+    }
+    return strings;
 }
 
 vector<long long> splitStringToInt(const string& str, char delim) {
-	vector<long long> strings;
-	size_t start;
-	size_t end = 0;
-	while ((start = str.find_first_not_of(delim, end)) != string::npos) {
-		end = str.find(delim, start);
-		strings.push_back(stoi(str.substr(start, end - start)));
-	}
-	return strings;
+    vector<long long> strings;
+    size_t start;
+    size_t end = 0;
+    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
+        end = str.find(delim, start);
+        strings.push_back(stoi(str.substr(start, end - start)));
+    }
+    return strings;
 }
 
 void printVector(vector<int> vec) {
-	for (vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
-		cout << *i << ' ';
-	}
-	cout << endl;
+    for (vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+        cout << *i << ' ';
+    }
+    cout << endl;
 }
 
 void printVector(vector<string> vec) {
-	for (vector<string>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
-		cout << *i << ' ';
-	}
-	cout << endl;
+    for (vector<string>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+        cout << *i << ' ';
+    }
+    cout << endl;
 }
 
 int main() {
-	string firstLine;
-	getline(cin, firstLine);
+    string firstLine;
+    getline(cin, firstLine);
 
-	vector<long long> firstLineVec = splitStringToInt(firstLine, ' ');
-	int n = firstLineVec[0];
-	int k = firstLineVec[1];
-	int d = firstLineVec[2];
-	int m = firstLineVec[3];
+    vector<long long> firstLineVec = splitStringToInt(firstLine, ' ');
+    int n = firstLineVec[0];
+    int k = firstLineVec[1];
+    int d = firstLineVec[2];
+    int m = firstLineVec[3];
 
-	string returns;
-	getline(cin, returns);
+    string returns;
+    getline(cin, returns);
 
-	vector<long long> returnsVec = splitStringToInt(returns, ' ');
+    vector<long long> returnsVec = splitStringToInt(returns, ' ');
 
-	long long result = calculateMaximizedReturns(n, k, d, m, returnsVec);
+    long long result = calculateMaximizedReturns(n, k, d, m, returnsVec);
 
-	// Do not remove below line
-	cout << result << "\n";
-	// Do not print anything after this line
+    // Do not remove below line
+    cout << result << "\n";
+    // Do not print anything after this line
 
-	return 0;
+    return 0;
 }
