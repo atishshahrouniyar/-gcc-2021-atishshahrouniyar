157caacf1d35f2035377ae153c8d8001c5217637
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2021-10-17 20:36:37 +0530

    COMMIT BY GCC 2021

--- Question1/Question1.cpp
+++ Question1/Question1.cpp
@@ -1,16 +1,9 @@
 
 using namespace std;
-#include <string>
 #include <iostream>
 
-int solution(int n){
-    return (n*2+1)/3;
-}
-
-/*  Do not edit below code */
 int main() {
 	int n;
 	cin >> n;	
-    int answer=solution(n);
-	cout << answer << endl;	
+	cout << (n*2+1)/3 << endl;	
 }
